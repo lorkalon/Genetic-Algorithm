8701b925cd4f789d11b4cbd2315dae379f56289c
lorkalon <lorkalon@mail.ru>
2013-04-03 18:45:52 +0300

    Added classes

---
+++ GeneticAlgoritm/EvenDivision.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgoritm
+{
+    class EvenDivision : IDividable
+    {
+        public List<List<IEntity>> DivideEntities(List<IEntity> entities)
+        {
+            List<List<IEntity>> groups = new List<List<IEntity>>();
+            List<IEntity> firstGroup = new List<IEntity>();
+            List<IEntity> secondGroup = new List<IEntity>();
+
+            for (int i = 0; i < entities.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    firstGroup.Add(entities[i]);
+                }
+                else
+                {
+                    secondGroup.Add(entities[i]);
+                }
+            }
+
+            groups.Add(firstGroup);
+            groups.Add(secondGroup);
+
+            return groups;
+        }
+    }
+
+}
